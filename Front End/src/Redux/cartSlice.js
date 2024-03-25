1de48d27e092d9bb8ee95b4bfcdba3a74faa88f8
Mohammad Zefri <128099271+zefrimohammad@users.noreply.github.com>
2024-03-25 00:44:01 +0000

    Update cartSlice.js

--- Front End/src/Redux/cartSlice.js
+++ Front End/src/Redux/cartSlice.js
@@ -8,17 +8,17 @@ const initialState = {
 export const counterSlice = createSlice({
   name: "cart",
   initialState,
-  //action.payload => product From Api => العنصر اللي بداخل الاقواس
+  
   reducers: {
     addToCart: (state, action) => {
-      //action.payload => product From Api => انا بضيف المنتج القادم من الايه بي اي
+      
       const productsWithQuantity = { ...action.payload, Quantity: 1 };
       state.selectedProducts.push(productsWithQuantity);
-      // state.selectedProductsID.push(action.payload.id);
+      
       localStorage.setItem("selectedProducts",JSON.stringify(state.selectedProducts))
     },
     increaseQuantity: (state, action) => {
-      //action.payload => product From user => انا بزود المنتج اللي المستخدم اختاره
+      
       const increaseProduct = state.selectedProducts.find(
         (product) => product.id === action.payload.id
       );
@@ -26,37 +26,32 @@ export const counterSlice = createSlice({
       localStorage.setItem("selectedProducts",JSON.stringify(state.selectedProducts))
     },
     decreaseQuantity: (state, action) => {
-      //action.payload => product From user
+      
       const decreaseProduct = state.selectedProducts.find(
         (product) => product.id === action.payload.id
       );
       decreaseProduct.Quantity -= 1;
-      //delete product if quantity = 0
+      
       if (decreaseProduct.Quantity === 0) {
         const newArr = state.selectedProducts.filter(
           (product) => product.id !== action.payload.id
         );
-        // const newArr2= state.selectedProductsID.filter(
-        //   (productId) => productId !== action.payload.id
-        // );
+        
         state.selectedProducts =newArr
-        // state.selectedProductsID = newArr2
+        
 
       }
       localStorage.setItem("selectedProducts",JSON.stringify(state.selectedProducts))
     },
 
     deleteProduct: (state, action) => {
-      //action.payload => product From user
+      
       const newArr = state.selectedProducts.filter(
         (product) => product.id !== action.payload.id
       );
-      // const newArr2= state.selectedProductsID.filter(
-      //   (productId) => productId !== action.payload.id
-      // );
+      
       state.selectedProducts =newArr
-      // state.selectedProductsID = newArr2
-      // deletedProduct
+      
       localStorage.setItem("selectedProducts",JSON.stringify(state.selectedProducts))
     },
   },
